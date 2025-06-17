40f77f05823c43966eedf4c1a435f58a03ebc4d3
Gustavo.Ardaya <gustavo.ardaya@gmail.com>
2025-06-17 20:17:53 +0200

    Set View Target on PlayerController

--- Source/FrontendUI/Private/Controllers/FrontendPlayerController.cpp
+++ Source/FrontendUI/Private/Controllers/FrontendPlayerController.cpp
@@ -3,3 +3,17 @@
 
 #include "Controllers/FrontendPlayerController.h"
 
+#include "Camera/CameraActor.h"
+#include "Kismet/GameplayStatics.h"
+
+void AFrontendPlayerController::OnPossess(APawn* aPawn)
+{
+	Super::OnPossess(aPawn);
+
+	TArray<AActor*> FoundCameras; 
+	UGameplayStatics::GetAllActorsOfClassWithTag(this, ACameraActor::StaticClass(), FName("Default"), FoundCameras);
+	if (!FoundCameras.IsEmpty())
+	{
+		SetViewTarget(FoundCameras[0]);
+	}
+}
--- Source/FrontendUI/Public/Controllers/FrontendPlayerController.h
+++ Source/FrontendUI/Public/Controllers/FrontendPlayerController.h
@@ -13,5 +13,11 @@ UCLASS()
 class FRONTENDUI_API AFrontendPlayerController : public APlayerController
 {
 	GENERATED_BODY()
+
+protected:
+
+	//~ Begin APlayerController Interface
+	virtual void OnPossess(APawn* aPawn) override;
+	//~ End APlayerController Interface
 	
 };
