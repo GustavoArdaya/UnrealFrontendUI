d9ea1bc0883d5fbe66f32a646db081f9dd3a3e00
Gustavo.Ardaya <gustavo.ardaya@gmail.com>
2025-06-18 15:33:33 +0200

    Implement async action

--- Source/FrontendUI/Private/AsyncActions/AsyncAction_PushSoftWidget.cpp
+++ Source/FrontendUI/Private/AsyncActions/AsyncAction_PushSoftWidget.cpp
@@ -3,9 +3,12 @@
 
 #include "AsyncActions/AsyncAction_PushSoftWidget.h"
 
+#include "Subsystems/FrontendUISubsystem.h"
+#include "Widgets/Widget_ActivatableBase.h"
+
 UAsyncAction_PushSoftWidget* UAsyncAction_PushSoftWidget::PushSoftWidget(const UObject* WorldContextObject,
-	APlayerController* OwningPlayerController, TSoftClassPtr<UWidget_ActivatableBase> InSoftWidgetClass,
-	UPARAM(meta = (Categories = "Frontend.WidgetStack")) FGameplayTag InWidgetStackTag, bool bFocusOnNewlyPushedWidget)
+                                                                         APlayerController* OwningPlayerController, TSoftClassPtr<UWidget_ActivatableBase> InSoftWidgetClass,
+                                                                         UPARAM(meta = (Categories = "Frontend.WidgetStack")) FGameplayTag InWidgetStackTag, bool bFocusOnNewlyPushedWidget)
 {
 	checkf(!InSoftWidgetClass.IsNull(), TEXT("PushSoftWidgetToStack was passed with a null soft widget class"));
 
@@ -14,10 +17,49 @@ UAsyncAction_PushSoftWidget* UAsyncAction_PushSoftWidget::PushSoftWidget(const U
 		if (UWorld* World = GEngine->GetWorldFromContextObject(WorldContextObject, EGetWorldErrorMode::LogAndReturnNull))
 		{
 			UAsyncAction_PushSoftWidget* Node = NewObject<UAsyncAction_PushSoftWidget>();
+			Node->CachedOwningWorld = World;
+			Node->CachedOwningPlayerController = OwningPlayerController;
+			Node->CachedSoftWidgetClass	= InSoftWidgetClass;
+			Node->CachedWidgetStackTag = InWidgetStackTag;
+			Node->bCachedFocusOnNewlyPushedWidget = bFocusOnNewlyPushedWidget;
+			
 			Node->RegisterWithGameInstance(World);
 			return Node;
 		}
 	}
 
 	return nullptr;
+}
+
+void UAsyncAction_PushSoftWidget::Activate()
+{
+	if (UFrontendUISubsystem* FrontendUISubsystem = UFrontendUISubsystem::Get(CachedOwningWorld.Get()))
+	{
+		FrontendUISubsystem->PushSoftWidgetToStackAsync(
+			CachedWidgetStackTag, CachedSoftWidgetClass,
+			[this](EAsyncPushWidgetState InPushState, UWidget_ActivatableBase* PushedWidget)
+			{
+				switch (InPushState)
+				{
+				case EAsyncPushWidgetState::OnCreatedBeforePush:
+					PushedWidget->SetOwningPlayer(CachedOwningPlayerController.Get());
+					OnWidgetCreatedBeforePush.Broadcast(PushedWidget);
+					break;
+				case EAsyncPushWidgetState::AfterPush:
+					AfterPush.Broadcast(PushedWidget);
+					if (bCachedFocusOnNewlyPushedWidget)
+					{
+						if (UWidget* WidgetToFocus = PushedWidget->GetDesiredFocusTarget())
+						{
+							WidgetToFocus->SetFocus();
+						}
+					}
+					SetReadyToDestroy();
+					break;
+				default:
+					break;
+				}
+			}
+		);
+	}
 }
--- Source/FrontendUI/Public/AsyncActions/AsyncAction_PushSoftWidget.h
+++ Source/FrontendUI/Public/AsyncActions/AsyncAction_PushSoftWidget.h
@@ -25,9 +25,22 @@ public:
 		APlayerController* OwningPlayerController, TSoftClassPtr<UWidget_ActivatableBase> InSoftWidgetClass,
 		UPARAM(meta = (Categories = "Frontend.WidgetStack")) FGameplayTag InWidgetStackTag, bool bFocusOnNewlyPushedWidget = true);
 
+	// ~ Begin UBlueprintAsyncActionBase Interface
+	virtual void Activate() override;
+	// ~ End UBlueprintAsyncActionBase Interface
+
 	UPROPERTY(BlueprintAssignable)
 	FOnPushSoftWidgetDelegate OnWidgetCreatedBeforePush;
 
 	UPROPERTY(BlueprintAssignable)
 	FOnPushSoftWidgetDelegate AfterPush;
+
+private:
+
+	TWeakObjectPtr<UWorld> CachedOwningWorld;
+	TWeakObjectPtr<APlayerController> CachedOwningPlayerController;
+	TSoftClassPtr<UWidget_ActivatableBase> CachedSoftWidgetClass;
+	FGameplayTag CachedWidgetStackTag;
+	bool bCachedFocusOnNewlyPushedWidget = false;
+	
 };
