ab6d500dcb7382ffd98f31799a84287ae0c6c5b8
Gustavo.Ardaya <gustavo.ardaya@gmail.com>
2025-06-19 14:11:57 +0200

    Add Common Button Base

---
+++ Source/FrontendUI/Private/Widgets/Components/FrontendCommonButtonBase.cpp
@@ -0,0 +1,19 @@
+// Vince Petrelli
+
+
+#include "Widgets/Components/FrontendCommonButtonBase.h"
+#include "CommonTextBlock.h"
+
+void UFrontendCommonButtonBase::SetButtonText(FText InText)
+{
+	if (CommonTextBlock_ButtonText && !InText.IsEmpty())
+	{
+		CommonTextBlock_ButtonText->SetText(bUseUpperCaseForButtonText ? InText.ToUpper() : InText);
+	}
+}
+
+void UFrontendCommonButtonBase::NativePreConstruct()
+{
+	Super::NativePreConstruct();
+	SetButtonText(ButtonDisplayText);
+}
