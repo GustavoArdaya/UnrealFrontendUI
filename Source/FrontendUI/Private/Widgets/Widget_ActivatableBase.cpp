4359775c7540d308e57727d8e693d683712f74e4
Gustavo.Ardaya <gustavo.ardaya@gmail.com>
2025-06-19 19:17:15 +0200

    On Any Key pressed logic

--- Source/FrontendUI/Private/Widgets/Widget_ActivatableBase.cpp
+++ Source/FrontendUI/Private/Widgets/Widget_ActivatableBase.cpp
@@ -2,4 +2,14 @@
 
 
 #include "Widgets/Widget_ActivatableBase.h"
+#include "Controllers/FrontendPlayerController.h"
 
+AFrontendPlayerController* UWidget_ActivatableBase::GetOwningFrontendPlayerController()
+{
+	if (!CachedOwningFrontendPlayerController.IsValid())
+	{
+		CachedOwningFrontendPlayerController = GetOwningPlayer<AFrontendPlayerController>();
+	}
+
+	return CachedOwningFrontendPlayerController.IsValid() ? CachedOwningFrontendPlayerController.Get() : nullptr;
+}
