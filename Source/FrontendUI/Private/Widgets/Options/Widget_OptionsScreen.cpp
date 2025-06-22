92b682f0461bc1f9cc1e547593c289724cd6377f
Gustavo.Ardaya <gustavo.ardaya@gmail.com>
2025-06-22 16:21:18 +0200

    Create custom bound action buttons

--- Source/FrontendUI/Private/Widgets/Options/Widget_OptionsScreen.cpp
+++ Source/FrontendUI/Private/Widgets/Options/Widget_OptionsScreen.cpp
@@ -3,3 +3,40 @@
 
 #include "Widgets/Options/Widget_OptionsScreen.h"
 
+#include "FrontendDebugHelper.h"
+#include "ICommonInputModule.h"
+#include "Input/CommonUIInputTypes.h"
+
+void UWidget_OptionsScreen::NativeOnInitialized()
+{
+	Super::NativeOnInitialized();
+
+	if (!ResetAction.IsNull())
+	{
+		ResetActionHandle = RegisterUIActionBinding(
+			FBindUIActionArgs(
+				ResetAction,
+				true,
+				FSimpleDelegate::CreateUObject(this, &ThisClass::OnResetBoundActionTriggered)
+			)
+		);
+	}
+
+	RegisterUIActionBinding(
+		FBindUIActionArgs(
+		ICommonInputModule::GetSettings().GetDefaultBackAction(),
+		true,
+		FSimpleDelegate::CreateUObject(this, &UWidget_OptionsScreen::OnBackBoundActionTriggered)
+		)
+	);
+}
+
+void UWidget_OptionsScreen::OnResetBoundActionTriggered()
+{
+	Debug::Print(TEXT("Reset Bound Action Triggered"));
+}
+
+void UWidget_OptionsScreen::OnBackBoundActionTriggered()
+{
+	DeactivateWidget();
+}
--- Source/FrontendUI/Public/Widgets/Options/Widget_OptionsScreen.h
+++ Source/FrontendUI/Public/Widgets/Options/Widget_OptionsScreen.h
@@ -13,5 +13,20 @@ UCLASS(Abstract, BlueprintType, meta = (DisableNaiveTick))
 class FRONTENDUI_API UWidget_OptionsScreen : public UWidget_ActivatableBase
 {
 	GENERATED_BODY()
-	
+
+protected:
+
+	//~ Begin UUserWidget Interface
+	virtual void NativeOnInitialized() override;
+	//~ End UUserWidget Interface
+
+private:
+
+	void OnResetBoundActionTriggered();
+	void OnBackBoundActionTriggered();
+
+	UPROPERTY(EditDefaultsOnly, Category = "Frontend Options Screen", meta = (RowType = "/Script/CommonUI.CommonInputActionDataBase"))
+	FDataTableRowHandle ResetAction;
+
+	FUIActionBindingHandle ResetActionHandle;
 };
