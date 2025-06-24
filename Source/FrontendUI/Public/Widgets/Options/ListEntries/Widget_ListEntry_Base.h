7166156113485bca9c71c68935af309901d3e380
Gustavo.Ardaya <gustavo.ardaya@gmail.com>
2025-06-24 17:22:29 +0200

    Add List Entry Widget Base

---
+++ Source/FrontendUI/Public/Widgets/Options/ListEntries/Widget_ListEntry_Base.h
@@ -0,0 +1,33 @@
+// Vince Petrelli
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CommonUserWidget.h"
+#include "Blueprint/IUserObjectListEntry.h"
+#include "Widget_ListEntry_Base.generated.h"
+
+class UCommonTextBlock;
+/**
+ * 
+ */
+UCLASS(Abstract, BlueprintType, meta = (DisableNaiveTick))
+class FRONTENDUI_API UWidget_ListEntry_Base : public UCommonUserWidget, public IUserObjectListEntry
+{
+	GENERATED_BODY()
+
+protected:
+
+	// ~ Begin IUserObjectListEntry Interface
+	virtual void NativeOnListItemObjectSet(UObject* ListItemObject) override;
+	// ~ End IUserObjectListEntry Interface
+
+private:
+
+	// ****** Bound Widgets ****** //
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidgetOptional, AllowPrivateAccess = "true"))
+	UCommonTextBlock* CommonText_SettingDisplayName;
+	
+	// ****** Bound Widgets ****** //
+	
+};
