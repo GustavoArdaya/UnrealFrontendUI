8cb1a9271ed34bd4f94a0eba6d75d9acfcf79928
Gustavo.Ardaya <gustavo.ardaya@gmail.com>
2025-06-25 13:00:11 +0200

    Override OnGenerateEntryWidgetInternal, in FronendCommonListView

--- Source/FrontendUI/Private/Widgets/Components/FrontendCommonListView.cpp
+++ Source/FrontendUI/Private/Widgets/Components/FrontendCommonListView.cpp
@@ -3,3 +3,36 @@
 
 #include "Widgets/Components/FrontendCommonListView.h"
 
+#include "Editor/WidgetCompilerLog.h"
+#include "Widgets/Options/DataAsset_DataListEntryMapping.h"
+#include "Widgets/Options/DataObjects/ListDataObject_Base.h"
+#include "Widgets/Options/ListEntries/Widget_ListEntry_Base.h"
+
+
+UUserWidget& UFrontendCommonListView::OnGenerateEntryWidgetInternal(UObject* Item,
+                                                                    TSubclassOf<UUserWidget> DesiredEntryClass, const TSharedRef<STableViewBase>& OwnerTable)
+{
+	if (IsDesignTime())
+	{
+		return Super::OnGenerateEntryWidgetInternal(Item, DesiredEntryClass, OwnerTable);
+	}
+
+	TSubclassOf<UWidget_ListEntry_Base> FoundWidgetclass = DataListEntryMapping->FindEntryWidgetClassByDataObject(CastChecked<UListDataObject_Base>(Item));
+
+	return GenerateTypedEntry<UWidget_ListEntry_Base>(FoundWidgetclass, OwnerTable);
+}
+
+#if WITH_EDITOR
+void UFrontendCommonListView::ValidateCompiledDefaults(IWidgetCompilerLog& CompileLog) const
+{
+	Super::ValidateCompiledDefaults(CompileLog);
+
+	if (!DataListEntryMapping)
+	{
+		CompileLog.Error(FText::FromString(TEXT("Variable DataListEntryMapping has no valid Data Asset assigned.") +
+			GetClass()->GetName() +
+			TEXT(" needs a valid Data Asset to function properly")
+		));
+	}
+}
+#endif
