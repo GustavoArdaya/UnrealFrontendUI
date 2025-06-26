eaaabba0bf3459074a6cd4d4088b3fceacad2aed
Gustavo.Ardaya <gustavo.ardaya@gmail.com>
2025-06-26 19:44:18 +0200

    Add OptionsDataInteractionHelper

---
+++ Source/FrontendUI/Public/Widgets/Options/OptionsDataInteractionHelper.h
@@ -0,0 +1,26 @@
+// Vince Petrelli
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PropertyPathHelpers.h"
+
+class UFrontendGameUserSettings;
+/**
+ * 
+ */
+class FRONTENDUI_API FOptionsDataInteractionHelper
+{
+public:
+
+	FOptionsDataInteractionHelper(const FString& InSetterOrGetterFuncPath);
+
+	FString GetValueAsString() const;
+	void SetValueFromString(const FString& InStringValue);
+	
+	
+private:
+
+	FCachedPropertyPath CachedDynamicFunctionPath;
+	TWeakObjectPtr<UFrontendGameUserSettings> CachedWeakGameUserSettings;
+};
