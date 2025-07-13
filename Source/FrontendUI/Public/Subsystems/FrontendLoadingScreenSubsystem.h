8786b177950bb8daaf2c727c2dacd8a3cbb45e8a
Gustavo.Ardaya <gustavo.ardaya@gmail.com>
2025-07-13 17:20:12 +0200

    Add ticking to LoadingScreen Subsystem

--- Source/FrontendUI/Public/Subsystems/FrontendLoadingScreenSubsystem.h
+++ Source/FrontendUI/Public/Subsystems/FrontendLoadingScreenSubsystem.h
@@ -10,7 +10,7 @@
  * 
  */
 UCLASS()
-class FRONTENDUI_API UFrontendLoadingScreenSubsystem : public UGameInstanceSubsystem
+class FRONTENDUI_API UFrontendLoadingScreenSubsystem : public UGameInstanceSubsystem, public FTickableGameObject
 {
 	GENERATED_BODY()
 
@@ -21,6 +21,14 @@ public:
 	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
 	virtual void Deinitialize() override;
 	// ~ End USubsystem Interface
+
+	// ~ Begin FTickableGameObject Interface
+	virtual UWorld* GetTickableGameObjectWorld() const override;
+	virtual void Tick(float DeltaTime) override;
+	virtual ETickableTickType GetTickableTickType() const override;
+	virtual bool IsTickable() const override;
+	virtual TStatId GetStatId() const override;
+	// ~ End FTickableGameObject Interface
 
 private:
 
